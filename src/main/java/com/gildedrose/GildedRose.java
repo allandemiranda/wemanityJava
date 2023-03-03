6273a6e1c6adcd90723fefcae5a53b3c922d1cce
Allan de Miranda Silva <allandemiranda@gmail.com>
2023-03-03 21:28:58 +0000

    Refracted code done, test ok, and some comments of coherence with the 'Gilded Rose Requirements Specification'

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,6 +1,15 @@
 package com.gildedrose;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+
 class GildedRose {
+
+  public static final String AGED_BRIE = "Aged Brie";
+  public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+  public static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+
   Item[] items;
 
   public GildedRose(Item[] items) {
@@ -8,53 +17,86 @@ class GildedRose {
   }
 
   public void updateQuality() {
-    for (Item item : items) {
-      if (!item.name.equals("Aged Brie") && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-        if (item.quality > 0) {
-          if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-            item.quality = item.quality - 1;
-          }
-        }
+    // parallel processor if Item's
+    Arrays.stream(items).parallel().forEach(item -> {
+      switch (item.name) {
+        case AGED_BRIE -> updateAgedBrieItem(item);
+        case SULFURAS -> updateSulfurasItem(item);
+        case BACKSTAGE_PASSES -> updateBackstagePassesItem(item);
+        default -> updateNormalItem(item);
+      }
+    });
+  }
+
+  /**
+   * Update normal item Quality.
+   *
+   * @param item the item
+   */
+  private void updateNormalItem(@NotNull Item item) {
+    item.sellIn--;
+    if (item.quality > 0) {
+      if (item.sellIn >= 0) {
+        item.quality--;
       } else {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1;
-
-          if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (item.sellIn < 11) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1;
-              }
-            }
-
-            if (item.sellIn < 6) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1;
-              }
-            }
-          }
-        }
-      }
-
-      if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-        item.sellIn = item.sellIn - 1;
-      }
-
-      if (item.sellIn < 0) {
-        if (!item.name.equals("Aged Brie")) {
-          if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (item.quality > 0) {
-              if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                item.quality = item.quality - 1;
-              }
-            }
-          } else {
-            item.quality = 0;
-          }
+        if (item.quality < 2) {
+          item.quality = 0;
         } else {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1;
-          }
+          item.quality -= 2;
         }
+      }
+    }
+  }
+
+  /**
+   * Update 'Aged Brie' Quality.
+   *
+   * @param item the item
+   */
+  private void updateAgedBrieItem(@NotNull Item item) {
+    item.sellIn--;
+    if (item.quality < 50) {
+      item.quality++;
+      // this part is in the code line 56, but not in the *Gilded Rose Requirements Specification*
+      if (item.sellIn < 0) {
+        item.quality++;
+      }
+    }
+  }
+
+  /**
+   * Update 'Sulfuras, Hand of Ragnaros' Quality.
+   *
+   * @param item the item
+   */
+  private void updateSulfurasItem(@NotNull Item item) {
+    item.quality = 80;
+  }
+
+  /**
+   * Update 'Backstage passes to a TAFKAL80ETC concert' Quality.
+   *
+   * @param item the item
+   */
+  private void updateBackstagePassesItem(@NotNull Item item) {
+    item.sellIn--;
+    if (item.sellIn < 0) {
+      item.quality = 0;
+    } else {
+      // *Gilded Rose Requirements Specification* says 5 days or less
+      if (item.sellIn < 5) {
+        item.quality += 3;
+      } else {
+        // *Gilded Rose Requirements Specification* says 10 days or less
+        if (item.sellIn < 10) {
+          item.quality += 2;
+        } else {
+          item.quality++;
+        }
+      }
+      // The problem is in the position where the decrease of the day is performed, line 40
+      if (item.quality > 50) {
+        item.quality = 50;
       }
     }
   }
